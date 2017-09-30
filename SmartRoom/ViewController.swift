7a601ca7b536528c225d1a70588d88a6c3833ab4
Breno Aquino <breno@MacBook-Pro-de-Breno.local>
2017-09-30 15:46:28 -0300

    Initial Commit

---
+++ SmartRoom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartRoom
+//
+//  Created by Breno Aquino on 30/09/2017.
+//  Copyright © 2017 Breno Aquino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
